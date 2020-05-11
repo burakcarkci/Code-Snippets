797dd6a3452e9bc49c295d0cee49f6cb77fba7d8
Burak <burakcarkci@hotmail.com>
2020-05-11 02:25:04 -0400

    Reverse String

---
+++ Reverse-String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// Given an input string, reverse the string word by word.
+
+namespace Exercises
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string[] inputString = "Hello my name is Jacob".Split(" ");
+
+            string reversedString = "";
+
+            for (int word = myString.Length - 1; word >= 0; word--)
+            {
+                reversedString += myString[word] + " ";
+            }
+            Console.WriteLine(reversedString);
+        }
+    }
+}
