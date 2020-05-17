08373c30e73659f6b0709636cae3758ed5f77090
Burak <burakcarkci@hotmail.com>
2020-05-17 14:33:06 -0400

    Median of Arrays

---
+++ Median-of-Arrays/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Find the median of two sorted arrays.
+// arr1 = [1, 3, 5]
+// arr2 = [2, 4, 6]
+// median(arr1, arr2) = 3.5
+
+namespace Exercises
+{
+    public class Median
+    {
+        public void MeadianValue(int[] arr1, int[] arr2)
+        {
+            List<int> newList = new List<int>();
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                newList.Add(arr1[i]);
+            }
+            for (int j = 0; j < arr2.Length; j++)
+            {
+                newList.Add(arr2[j]);
+            }
+
+            int[] arr3 = newList.ToArray();
+
+            int len = arr3.Length;
+
+            if (len % 2 == 0)
+            {
+                int val1 = arr3[len / 2];
+                int val2 = arr3[len / 2 - 1];
+                int sum = val1 + val2;
+
+                double even = (double)sum / 2;
+
+                Console.WriteLine(even);
+            }
+            else
+            {
+                int odd = arr3[len / 2];
+
+                Console.WriteLine(odd);
+            }
+        }
+    }
+
+    internal partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Median median = new Median();
+            int[] array1 = { 1, 2, 3, 4 };
+            int[] array2 = { 5, 6, 8, 12 };
+            median.MeadianValue(array1, array2);
+        }
+    }
+}
